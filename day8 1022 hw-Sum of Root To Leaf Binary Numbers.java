0758052c6a4a1aeb96ee9ae2a769fd9b8da3be29
js zubair <jszubair007@gmail.com>
2026-07-23 08:47:24 +0530

    Create day8 1022 hw-Sum of Root To Leaf Binary Numbers.java

---
+++ day8 1022 hw-Sum of Root To Leaf Binary Numbers.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int currentSum) {
+        if (node == null) {
+            return 0;
+        }
+
+        // Shift left by 1 (multiply by 2) and add the current node's value
+        currentSum = (currentSum << 1) | node.val;
+
+        // If it's a leaf node, return the binary path sum
+        if (node.left == null && node.right == null) {
+            return currentSum;
+        }
+
+        // Recursively calculate sum for left and right children
+        return dfs(node.left, currentSum) + dfs(node.right, currentSum);
+    }
+}
